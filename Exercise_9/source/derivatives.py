ab3a4297a8efa081ed8d12a88df6779aa914ad12
Gullik Vetvik Killie <gullik.killie@gmail.com>
2015-08-18 15:48:36 +0200

    Progress on exercise 9

--- Exercise_9/source/derivatives.py
+++ Exercise_9/source/derivatives.py
@@ -1,14 +1,14 @@
 import sympy as sp
 
 x, y, z, = sp.symbols('x, y, z')
-mu, mz = sp.symbols('mu, mz')
+mu_0, m_z = sp.symbols('mu_0, m_z')
 
 
 r = sp.sqrt(x*x + y*y + z*z)
 
-Bz =	mu*mz / (4*sp.pi) * ( 3*z*r**-5 - r**-3  )
+Bz =	mu_0*m_z / (4*sp.pi) * ( 3*z*z*r**-5 - r**-3  )
 
 R = sp.Symbol('r')
 
 
-print(sp.latex( (sp.diff(Bz, z)).subs(r,R)  ))
+print(sp.latex( (sp.diff(Bz, z)).subs(r,R).simplify()  ))
--- Exercise_9/source/stability.py
+++ Exercise_9/source/stability.py
@@ -1,23 +1,146 @@
 import numpy as np
 import pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def r_length(x,y,z):
-	return (x*x + y*y +z*z)**.5
+	return np.sqrt(x*x + y*y +z*z)
 
-def dipMomentz(z):
-	return 7.94E22
+def dipMomentz():
+	return 7.94E22			#A m^2
 
 def bx(x,y,z, C2):
-	mz = dipMomentz(z)
+	mz = dipMomentz()
 	return C2*(3.*x*(mz*z))/r_length(x,y,z)**5
 	# return 0
 
 def by(x,y,z,C2):
-	mz = dipMomentz(z)
+	mz = dipMomentz()
 	return C2*(3.*y*(mz*z))/r_length(x,y,z)**5
 	# return 0
 
 def bz(x,y,z,C2):
-	mz = dipMomentz(z)
+	mz = dipMomentz()
 	length = r_length(x,y,z)
 	return C2*((3.*z*(mz*z))*length**-5 - mz*length**-3)
+
+def b_magnitude(x,y,z, C):
+
+	Bx = bx(x,y,z, C)
+	By = by(x,y,z, C)
+	Bz = bz(x,y,z, C)
+
+	return r_length(Bx,By,Bz)
+
+def gx(x,y,z, C):
+	return C*x/r_length(x,y,z)**3
+
+def gy(x,y,z, C):
+	return C*y/r_length(x,y,z)**3
+
+def gz(x,y,z, C):
+	return C*z*r_length(x,y,z)**-3
+
+def dB_zdz(z, C):
+	return -C*z**-4
+
+def crossx(vx,vy,vz, Bx,By,Bz):
+	#Returns the x component of the cross product between two vectors
+	return vy*Bz - vz*By
+
+def crossy(vx,vy,vz, Bx,By,Bz):	
+	return vz*Bx - vx*Bz
+
+def crossz(vx,vy,vz, Bx,By,Bz):
+	return vx*By - vy*Bx
+
+def force(x,y,z, vx,vy,vz, C1, C2, C3, C4):
+	Bx = bx( x,y,z , C2)
+	By = by( x,y,z , C2)
+	Bz = bz( x,y,z , C2)
+
+	force_x = crossx(vx,vy,vz, Bx, By,Bz)*C4
+	force_y = crossy(vx,vy,vz, Bx, By,Bz)*C4
+	force_z = crossy(vx,vy,vz, Bx, By,Bz)*C4
+	
+	force_x -= gx( x,y,z , C1)
+	force_y -= gy( x,y,z , C1)
+	force_z -= gz( x,y,z , C1)
+
+	return force_x, force_y, force_z
+
+def euler(force, x, y, z, vx, vy, vz,C1, C2, C3, C4, nSteps, timestep ):
+	
+	positions = np.zeros((nSteps,3))
+
+	i = 0
+	while i < nSteps:
+
+		positions[i,:] = [x,y,z]
+		force_x, force_y, force_z = force( x,y,z, vx,vy,vz, C1, C2, C3, C4)
+
+		#Update positions
+		x += timestep*vx
+		y += timestep*vy
+		z += timestep*vz
+
+		#Update v
+		vx += timestep*force_x
+		vy += timestep*force_y 
+		vz += timestep*force_z
+
+		i += 1
+
+	#Plots
+	time = np.linspace(0, nSteps*timestep, nSteps)
+	
+	#3D plot of the trajectory
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	plt.suptitle('Particle trajectory')
+	ax.plot(positions[:,0],positions[:,1],positions[:,2])
+	ax.set_xlabel('x [m]')
+	ax.set_ylabel('y [m]')
+	ax.set_zlabel('z [m]')
+
+	ax.set_aspect('equal', 'datalim')
+
+	fig.savefig('../figures/3Dplot.eps')
+
+
+	return 0 #positions
+
+if __name__ == '__main__':
+
+	mu0 	= 4.*np.pi*1.E-7	#N/A^2
+	Re 		= 6.371E6			#m
+	q 		= -1.602E-19			#C
+	m_e 	= 9.109E-31			#kg	
+	Me 		= 5.9E24			#kg
+	gamma 	= 6.674E-11			#Nm^2/kg^2 
+	pitch	= np.deg2rad(90)	#radian
+
+	timestep 	= 1.E-11
+	nSteps 		= 1.E6
+
+	C1 = gamma*Me
+	C2 = mu0/(4*np.pi)	#Constant in the magnetic field calculation
+	C3 = 3*mu0*dipMomentz()/(2*np.pi)
+	C4 = q/m_e
+
+	#Initial position
+	x = 0
+	y = 0
+	z = 200.E3 + Re				#m
+
+	#Calculating a velocity that will balance the forces
+	v_perp = 2*b_magnitude(x,y,z, C2)*gz(x,y,z,C1)/dB_zdz(z,C3)
+
+	vx = v_perp*np.cos(pitch)
+	vy = v_perp*np.sin(pitch)
+	vz = 0
+
+	euler(force, x, y, z, vx, vy, vz, C1, C2, C3, C4, nSteps, timestep )
+
+	plt.show()
+
